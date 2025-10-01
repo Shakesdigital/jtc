ab1a54995b7520648f79b557a3d57b4c6e3873ca
Shakesdigital <shakesdigital@gmail.com>
2025-10-01 12:36:22 +0300

    Fix: Remove unused motion import from ArticleDetail.js to resolve build error

--- src/pages/ArticleDetail.js
+++ src/pages/ArticleDetail.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
 import {
   FiArrowLeft,
   FiCalendar,
